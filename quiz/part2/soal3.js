11dca03e79214be947b5c4bcff0cbea706d828b5
Rahmat Eka <155574253+rhmatzeka@users.noreply.github.com>
2026-02-24 14:50:55 +0700

    part2 selesai

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,11 @@
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+let exampleFirstWord4 = word3.substring(3, 13);
+let exampleFirstWord5 = word3.substring(13, 17);
+let exampleFirstWord6 = word3.substring(17, 20);
+let exampleFirstWord7 = word3.substring(20, 26);
+
+console.log('First Word: ' + exampleFirstWord3  + exampleFirstWord4 + exampleFirstWord5 + exampleFirstWord6 + exampleFirstWord7);
---
+++ quiz/part2/soal4.js
@@ -0,0 +1,20 @@
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+
+let exampleFirstWord4 = word4.substring(0, 3);
+let exampleFirstWord5 = word4.substring(4, 14);
+let exampleFirstWord6 = word4.substring(15, 17);
+let exampleFirstWord7 = word4.substring(18, 20);
+let exampleFirstWord8 = word4.substring(21, 26);
+
+
+let firstWordLength = word4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + exampleFirstWord4.length);
+console.log('Second Word: ' + exampleFirstWord5 + ', with length: ' + exampleFirstWord5.length);
+console.log('Third Word: ' + exampleFirstWord6 + ', with length: ' + exampleFirstWord6.length);
+console.log('Fourth Word: ' + exampleFirstWord7 + ', with length: ' + exampleFirstWord7.length);
+console.log('Fifth Word: ' + exampleFirstWord8 + ', with length: ' + exampleFirstWord8.length);
