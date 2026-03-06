a4301fa95eb8188f44d242777ec2227f7462273f
Rahmat Eka <matsganz@gmail.com>
2026-03-06 12:05:33 +0700

    baru selesai setengah

---
+++ quiz/ujian/soal2.js
@@ -0,0 +1,9 @@
+function balikKata(kata) {
+    return kata.split('').reverse().join('');
+}
+  
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); 
